93df250a5c419faa36b30e3c5b3ac541a9e48cb3
7c00 <i@7c00.cc>
2020-12-21 15:24:56 +0800

    添加 antd 用法

--- index.tsx
+++ index.tsx
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { Alert, Space } from "ant-design-vue";
 import Bar from "./bar";
 
 interface FooProps {
@@ -10,13 +11,16 @@ const Foo = ({ text, onPressed }: FooProps) => (
   <p onClick={() => onPressed(Math.random())}>{text}</p>
 );
 
-createApp({
+const app = createApp({
   render() {
     return (
-      <>
+      <Space style={{ padding: "16px" }} direction="vertical">
+        <Alert message="antd alert" showIcon />
         <Foo text="functional component" onPressed={(value) => console.log(value)} />
         <Bar title="defined component" onClick={(value) => console.log(value)} />
-      </>
+      </Space>
     );
   },
-}).mount("main");
+});
+
+app.mount("main");
